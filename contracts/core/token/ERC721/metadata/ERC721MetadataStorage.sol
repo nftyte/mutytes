8dbe6a5852e8345965d8bb6dd4dfd367c6c34a95
NFTicks <nfticks@gmail.com>
2022-06-18 01:10:48 +0300

    update docs

--- contracts/core/token/ERC721/metadata/ERC721MetadataStorage.sol
+++ contracts/core/token/ERC721/metadata/ERC721MetadataStorage.sol
@@ -2,10 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-// import { ERC721MetadataUtils } from "./utils/ERC721MetadataUtils.sol";
-
-// using ERC721MetadataUtils for ERC721MetadataStorage global;
-
 bytes32 constant ERC721_METADATA_STORAGE_SLOT = keccak256("core.token.erc721.metadata.storage");
 
 struct ERC721MetadataStorage {
