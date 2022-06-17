befaf046f120335b298e49991f37f01fde77a9cc
NFTicks <nfticks@gmail.com>
2022-06-17 19:49:23 +0300

    add erc721 metadata init constructor

--- contracts/core/token/ERC721/metadata/upgradable/ERC721MetadataUpgradable.sol
+++ contracts/core/token/ERC721/metadata/upgradable/ERC721MetadataUpgradable.sol
@@ -3,9 +3,15 @@ pragma solidity ^0.8.0;
 
 import { IERC721MetadataUpgradable as IERC721Metadata } from "./IERC721MetadataUpgradable.sol";
 import { UpgradableProxy } from "../../../../proxy/upgradable/UpgradableProxy.sol";
-import { erc721MetadataStorage as es } from "../ERC721MetadataStorage.sol";
+import { erc721MetadataStorage as es, ERC721MetadataStorage } from "../ERC721MetadataStorage.sol";
 
 abstract contract ERC721MetadataUpgradable is IERC721Metadata, UpgradableProxy {
+    constructor(string memory name_, string memory symbol_) {
+        ERC721MetadataStorage storage erc721Metadata = es();
+        erc721Metadata.name = name_;
+        erc721Metadata.symbol = symbol_;
+    }
+
     function name() external virtual override upgradable returns (string memory) {
         return es().name;
     }
