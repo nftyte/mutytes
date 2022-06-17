498566c1d68d5a3710fcd82e05895457082c0ce5
NFTicks <nfticks@gmail.com>
2022-06-17 19:28:23 +0300

    update erc721 upgradable

--- contracts/core/token/ERC721/upgradable/ERC721Upgradable.sol
+++ contracts/core/token/ERC721/upgradable/ERC721Upgradable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { IERC721 } from "../IERC721.sol";
+import { IERC721Upgradable as IERC721 } from "./IERC721Upgradable.sol";
 import { UpgradableProxy } from "../../../proxy/upgradable/UpgradableProxy.sol";
 import { erc721Storage as es, ERC721Storage } from "../ERC721Storage.sol";
 
--- contracts/core/token/ERC721/upgradable/IERC721Upgradable.sol
+++ contracts/core/token/ERC721/upgradable/IERC721Upgradable.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IERC721 /* is IERC165 */ {
+interface IERC721Upgradable /* is IERC165 */ {
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
 
     event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
 
     event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
 
-    function balanceOf(address owner) external view returns (uint256);
+    function balanceOf(address owner) external returns (uint256);
 
-    function ownerOf(uint256 tokenId) external view returns (address);
+    function ownerOf(uint256 tokenId) external returns (address);
 
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
 
@@ -22,7 +22,7 @@ interface IERC721 /* is IERC165 */ {
 
     function setApprovalForAll(address operator, bool approved) external;
 
-    function getApproved(uint256 tokenId) external view returns (address);
+    function getApproved(uint256 tokenId) external returns (address);
 
-    function isApprovedForAll(address owner, address operator) external view returns (bool);
+    function isApprovedForAll(address owner, address operator) external returns (bool);
 }
