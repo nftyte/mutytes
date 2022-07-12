399e1389ac04f7f8c88dd6a9bd0882b1923fd694
NFTicks <nfticks@gmail.com>
2022-07-12 13:17:33 +0300

    update mock classes

---
+++ contracts/TestERC721.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { ERC721Proxy } from "./core/token/erc721/ERC721Proxy.sol";
+import { ERC721MetadataProxy } from "./core/token/erc721/metadata/ERC721MetadataProxy.sol";
+import { ERC721EnumerableProxy } from "./core/token/erc721/enumerable/ERC721EnumerableProxy.sol";
+import { TestProxy } from "./TestProxy.sol";
+
+contract TestERC721 is
+    ERC721Proxy,
+    ERC721MetadataProxy,
+    ERC721EnumerableProxy,
+    TestProxy
+{
+    constructor(address init, bytes memory data) TestProxy(init, data) {}
+}
--- contracts/TestProxy.sol
+++ contracts/TestProxy.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 import { Proxy } from "./core/proxy/Proxy.sol";
-import { ProxyFaceted as Faceted } from "./core/proxy/faceted/ProxyFaceted.sol";
+import { ProxyInit as Initializable } from "./core/proxy/ProxyInit.sol";
 import { ProxyUpgradable as Upgradable } from "./core/proxy/upgradable/ProxyUpgradable.sol";
-import { ProxyInitializable as Initializable } from "./core/proxy/initializable/ProxyInitializable.sol";
+import { ProxyFacetedController as Faceted } from "./core/proxy/faceted/ProxyFacetedController.sol";
 
 contract TestProxy is Proxy, Initializable, Upgradable, Faceted {
     constructor(address init, bytes memory data) Initializable(init, data) {}
