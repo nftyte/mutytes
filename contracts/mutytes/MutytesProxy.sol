029762332fcc056081eb3a584910578348c92585
NFTicks <nfticks@gmail.com>
2022-07-12 22:15:48 +0300

    organize imports

--- contracts/mutytes/MutytesProxy.sol
+++ contracts/mutytes/MutytesProxy.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.0;
 
 import { MutytesTokenProxy } from "./token/MutytesTokenProxy.sol";
-import { Proxy } from "../core/proxy/Proxy.sol";
-import { ProxyInit } from "../core/proxy/ProxyInit.sol";
-import { ProxyFaceted } from "../core/proxy/faceted/ProxyFaceted.sol";
 import { SafeOwnableProxy } from "../core/access/ownable/safe/SafeOwnableProxy.sol";
+import { ProxyFaceted } from "../core/proxy/faceted/ProxyFaceted.sol";
+import { ProxyInit } from "../core/proxy/ProxyInit.sol";
+import { Proxy } from "../core/proxy/Proxy.sol";
 
 contract MutytesProxy is
     MutytesTokenProxy,
-    Proxy,
-    ProxyInit,
+    SafeOwnableProxy,
     ProxyFaceted,
-    SafeOwnableProxy
+    ProxyInit,
+    Proxy
 {
     constructor(address init, bytes memory data) ProxyInit(init, data) {}
 }
