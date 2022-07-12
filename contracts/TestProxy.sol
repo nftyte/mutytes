d3b44445c012978fff67909eb176ea26d0e08cdb
NFTicks <nfticks@gmail.com>
2022-07-12 14:20:54 +0300

    add shorthand ProxyFaceted

--- contracts/TestProxy.sol
+++ contracts/TestProxy.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import { Proxy } from "./core/proxy/Proxy.sol";
 import { ProxyInit as Initializable } from "./core/proxy/ProxyInit.sol";
 import { ProxyUpgradable as Upgradable } from "./core/proxy/upgradable/ProxyUpgradable.sol";
-import { ProxyFacetedController as Faceted } from "./core/proxy/faceted/ProxyFacetedController.sol";
+import { ProxyFaceted as Faceted } from "./core/proxy/faceted/ProxyFaceted.sol";
 
 contract TestProxy is Proxy, Initializable, Upgradable, Faceted {
     constructor(address init, bytes memory data) Initializable(init, data) {}
