2af92040dc68fcba1a0c57eb59ae3b9f2d7739ad
NFTicks <nfticks@gmail.com>
2022-07-12 13:12:58 +0300

    move upgradable modifier to controller

--- contracts/core/proxy/upgradable/ProxyUpgradable.sol
+++ contracts/core/proxy/upgradable/ProxyUpgradable.sol
@@ -1,16 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { ProxyController } from "../ProxyController.sol";
+import { ProxyUpgradableController } from "./ProxyUpgradableController.sol";
 
-abstract contract ProxyUpgradable is ProxyController {
-    modifier upgradable() {
-        address implementation = implementation_();
-        
-         if (implementation == address(this)) {
-            _;
-        } else {
-            _delegate(implementation);
-        }
-    }
-}
+abstract contract ProxyUpgradable is ProxyUpgradableController {}
