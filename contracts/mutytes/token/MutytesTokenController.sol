82ffc18f452e56edb3dde0baecc55cc0406273f6
NFTicks <nfticks@gmail.com>
2022-07-13 18:14:39 +0300

    enforce free mint

--- contracts/mutytes/token/MutytesTokenController.sol
+++ contracts/mutytes/token/MutytesTokenController.sol
@@ -2,17 +2,27 @@
 pragma solidity ^0.8.0;
 
 import { ERC721TokenURIModel } from "../../core/token/ERC721/tokenURI/ERC721TokenURIModel.sol";
+import { ERC721MintableController } from "../../core/token/ERC721/mintable/ERC721MintableController.sol";
 import { ERC721BurnableController } from "../../core/token/ERC721/burnable/ERC721BurnableController.sol";
+import { IntegerUtils } from "../../core/utils/IntegerUtils.sol";
 
 abstract contract MutytesTokenController is
     ERC721TokenURIModel,
+    ERC721MintableController,
     ERC721BurnableController
 {
+    using IntegerUtils for uint256;
+
     function _burn_(address owner, uint256 tokenId) internal virtual override {
         if (_tokenURIProvider(tokenId) != 0) {
             _setTokenURIProvider(tokenId, 0);
         }
 
         super._burn_(owner, tokenId);
+    }
+
+    function _enforceCanMint(uint256 amount) internal view virtual override {
+        msg.value.enforceIsZero();
+        super._enforceCanMint(amount);
     }
 }
