8261049ef40bf9ac1c9adde0b62a89fef38fdfca
NFTicks <nfticks@gmail.com>
2022-07-14 14:51:37 +0300

    fix mintable import

--- contracts/core/token/ERC721/mintable/safe/ERC721SafeMintableProxy.sol
+++ contracts/core/token/ERC721/mintable/safe/ERC721SafeMintableProxy.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.0;
 
 import { IERC721SafeMintable } from "./IERC721SafeMintable.sol";
-import { ERC721MintableController } from "../ERC721MintableController.sol";
+import { ERC721SafeMintableController } from "./ERC721SafeMintableController.sol";
 import { ProxyUpgradable } from "../../../../proxy/upgradable/ProxyUpgradable.sol";
 
 abstract contract ERC721SafeMintableProxy is
     IERC721SafeMintable,
-    ERC721MintableController,
+    ERC721SafeMintableController,
     ProxyUpgradable
 {
     function safeMint(uint256 amount, bytes calldata data)
