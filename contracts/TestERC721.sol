02f4fcfcb7fe5d4b5dd62a6cc899cd3a6f97c3ab
NFTicks <nfticks@gmail.com>
2022-07-16 16:08:42 +0300

    fix casing

--- contracts/TestERC721.sol
+++ contracts/TestERC721.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { ERC721Proxy } from "./core/token/erc721/ERC721Proxy.sol";
-import { ERC721MetadataProxy } from "./core/token/erc721/metadata/ERC721MetadataProxy.sol";
-import { ERC721EnumerableProxy } from "./core/token/erc721/enumerable/ERC721EnumerableProxy.sol";
+import { ERC721Proxy } from "./core/token/ERC721/ERC721Proxy.sol";
+import { ERC721MetadataProxy } from "./core/token/ERC721/metadata/ERC721MetadataProxy.sol";
+import { ERC721EnumerableProxy } from "./core/token/ERC721/enumerable/ERC721EnumerableProxy.sol";
 import { TestProxy } from "./TestProxy.sol";
 
 contract TestERC721 is
