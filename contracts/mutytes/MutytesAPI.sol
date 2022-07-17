9040b921e69add554ba206224ddebfb9dab7f2b4
NFTicks <nfticks@gmail.com>
2022-07-17 23:51:25 +0300

    make api class abstract

--- contracts/mutytes/MutytesAPI.sol
+++ contracts/mutytes/MutytesAPI.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import { MutytesToken } from "./token/MutytesToken.sol";
 import { SafeOwnable } from "../core/access/ownable/safe/SafeOwnable.sol";
 
-contract MutytesAPI is MutytesToken, SafeOwnable {
+abstract contract MutytesAPI is MutytesToken, SafeOwnable {
     constructor() {
         MutytesToken_();
         Ownable_();
