a04f16a056b0935b4f99310fdd0f95e548d990fa
NFTicks <nfticks@gmail.com>
2022-07-19 00:44:22 +0300

    add natspec

--- contracts/mutytes/MutytesAPI.sol
+++ contracts/mutytes/MutytesAPI.sol
@@ -5,6 +5,9 @@ pragma solidity ^0.8.0;
 import { MutytesToken } from "./token/MutytesToken.sol";
 import { SafeOwnable } from "../core/access/ownable/safe/SafeOwnable.sol";
 
+/**
+ * @title Mutytes proxy API implementation
+ */
 abstract contract MutytesAPI is MutytesToken, SafeOwnable {
     constructor() {
         MutytesToken_();
