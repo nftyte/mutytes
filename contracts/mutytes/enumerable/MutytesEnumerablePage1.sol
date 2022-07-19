141a9bbec075f487563823dc3a754eff9e697407
NFTicks <nfticks@gmail.com>
2022-07-19 18:11:36 +0300

    update enumerable test

--- contracts/mutytes/enumerable/MutytesEnumerablePage1.sol
+++ contracts/mutytes/enumerable/MutytesEnumerablePage1.sol
@@ -9,7 +9,7 @@ import { MutytesEnumerablePage } from "./MutytesEnumerablePage.sol";
  * @dev Note: Test implementation
  */
 contract MutytesEnumerablePage1 is MutytesEnumerablePage {
-    function _bytecode() internal view virtual override returns (bytes memory) {
+    function _bytecode() internal pure virtual override returns (bytes memory) {
         return
             hex"f39fd6e51aad88f6f4ce6ab8827279cfffb9226600f39fd6e51aad88f6f4ce6ab8827279cfffb9226601f39fd6e51aad88f6f4ce6ab8827279cfffb922660270997970c51812dc3a010c7d01b50e0d17dc79c80070997970c51812dc3a010c7d01b50e0d17dc79c80170997970c51812dc3a010c7d01b50e0d17dc79c80270997970c51812dc3a010c7d01b50e0d17dc79c803"; // 7 tokens
     }
--- contracts/mutytes/enumerable/MutytesEnumerablePage2.sol
+++ contracts/mutytes/enumerable/MutytesEnumerablePage2.sol
@@ -9,7 +9,7 @@ import { MutytesEnumerablePage } from "./MutytesEnumerablePage.sol";
  * @dev Note: Test implementation
  */
 contract MutytesEnumerablePage2 is MutytesEnumerablePage {
-    function _bytecode() internal view virtual override returns (bytes memory) {
+    function _bytecode() internal pure virtual override returns (bytes memory) {
         return
             hex"f39fd6e51aad88f6f4ce6ab8827279cfffb9226603f39fd6e51aad88f6f4ce6ab8827279cfffb92266043c44cdddb6a900fa2b585dd299e03d12fa4293bc003c44cdddb6a900fa2b585dd299e03d12fa4293bc013c44cdddb6a900fa2b585dd299e03d12fa4293bc023c44cdddb6a900fa2b585dd299e03d12fa4293bc03"; // 6 tokens
     }
